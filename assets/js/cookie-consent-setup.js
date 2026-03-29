7b868c6310f94c9e16da56f68f6db6bb22b6d1b3
ccnukoishi514 <168714247+ccnukoishi514@users.noreply.github.com>
2026-03-29 04:15:00 +0000

    Deploying to gh-pages from @ ccnukoishi514/ccnukoishi514.github.io@fb6b4c6d83288fe6e5b32aa9f763daddc9283b47 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://ccnukoishi514.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://koishi.site/#contact">contact us</a>.'
               }
             ]
           }
